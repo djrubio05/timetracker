dca7ef96c4a35d5e67765bcea7b2d4b458536cdf
lastletter05 <djrubio05@gmail.com>
2025-05-21 14:39:48 +0800

    added time entry form success url and save entry logic

--- myproject/timetracker/views.py
+++ myproject/timetracker/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormMixin
+from django.urls import reverse
+from django.contrib import messages
 
 from .forms import TimeEntryForm
 
@@ -19,3 +21,19 @@ class ProjectsListView(ListView):
 class TimeEntryDetailView(FormMixin, DetailView):
     model = Project
     form_class = TimeEntryForm
+
+    def get_success_url(self):
+        return reverse("timetracker:time_entry", args=(self.object.id,))
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = self.object
+            task.save()
+            messages.success(
+                self.request, "Time Entry added.")
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
