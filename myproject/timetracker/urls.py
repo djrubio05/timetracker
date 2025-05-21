0a73544845dcb54f1ea7914204bb9c11ab783445
lastletter05 <djrubio05@gmail.com>
2025-05-21 13:50:42 +0800

    used classview for projects and added placeholder template

--- myproject/timetracker/urls.py
+++ myproject/timetracker/urls.py
@@ -6,6 +6,6 @@ app_name = "timetracker"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("projects/", views.projects, name="projects"),
+    path("projects/", views.ProjectsListView.as_view(), name="projects"),
     path("projects/<int:project_id>", views.tasks, name="tasks"),
 ]
--- myproject/timetracker/views.py
+++ myproject/timetracker/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Project
 
 
 def index(request):
@@ -12,3 +15,7 @@ def projects(request):
 
 def tasks(request, project_id):
     return HttpResponse(f"You are at tasks for project:{project_id}")
+
+
+class ProjectsListView(ListView):
+    model = Project
