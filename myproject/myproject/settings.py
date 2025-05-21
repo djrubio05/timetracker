8a501f0b81c800c63a1014c42ed74d6a0c20e870
lastletter05 <djrubio05@gmail.com>
2025-05-21 13:17:42 +0800

    change database to postgresql in settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "timetracker",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
