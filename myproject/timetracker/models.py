f522503e5cfdc5b8e1141ff159aad8da1c2df954
lastletter05 <djrubio05@gmail.com>
2025-05-21 13:39:18 +0800

    create projects and tasks model, and initial migrate

--- myproject/timetracker/models.py
+++ myproject/timetracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TimeEntry(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    worked_hours = models.DecimalField(
+        "Hours Worked", max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
