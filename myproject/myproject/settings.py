5c10a4a226c8e7458761d0012bdffe0109995185
lastletter05 <djrubio05@gmail.com>
2025-05-21 13:21:27 +0800

    changed database pass for development

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "timetracker",
         "USER": "postgres",
-        "PASSWORD": "password",
+        "PASSWORD": "2545",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
