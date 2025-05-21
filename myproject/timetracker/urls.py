4f0e76d0912341c906c88d4b5c6f520dea242047
lastletter05 <djrubio05@gmail.com>
2025-05-21 13:31:52 +0800

    added path for projects and tasks

--- myproject/timetracker/urls.py
+++ myproject/timetracker/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "timetracker"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("projects/", views.projects, name="projects"),
+    path("projects/<int:project_id>", views.tasks, name="tasks"),
 ]
--- myproject/timetracker/views.py
+++ myproject/timetracker/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("You are at index")
+
+
+def projects(request):
+    return HttpResponse("You are at projects")
+
+
+def tasks(request, project_id):
+    return HttpResponse(f"You are at tasks for project:{project_id}")
